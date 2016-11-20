b18d3d10dede4f66f7b46079b18340d3a7e44ec6
Radoslav Mladenov <rvmladenov@gmail.com>
2016-11-20 18:43:52 +0200

    forgot to rebuild :)

--- assets/app/errors/error.service.ts
+++ assets/app/errors/error.service.ts
@@ -6,7 +6,7 @@ export class ErrorService {
     errorOccurred = new EventEmitter<Error>();
 
     handleError(error: any) {
-        const errorData = new Error(error.title, error.error.message);
+        const errorData = new Error(error.title, error.message);
         this.errorOccurred.emit(errorData);
     }
 }
