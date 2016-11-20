d5c25cb1f94da160b0edc9a621741d1f7cc66168
Radoslav Mladenov <rvmladenov@gmail.com>
2016-11-20 18:09:09 +0200

    bla

--- public/js/app/cpus/cpu.model.js
+++
@@ -1,8 +0,0 @@
-export var Cpu = (function () {
-    function Cpu(id, percentUsage) {
-        this.id = id;
-        this.percentUsage = percentUsage;
-    }
-    return Cpu;
-}());
-//# sourceMappingURL=cpu.model.js.map
